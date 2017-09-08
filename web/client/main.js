8739fb78980d694d15e608910c034dd4dca74371
Vaclavrut <vaclav@smartemailing.cz>
2017-09-08 21:08:52 +0200

    added searching

--- web/client/main.js
+++ web/client/main.js
@@ -3,6 +3,7 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';
 import './main.css';
 import { Posts } from '../both/db';
+import { Session } from 'meteor/session';
 
 GAnalytics.pageview();
 
@@ -12,21 +13,51 @@ Template.main.onCreated(() => {
         template.subscribe('posts');
     });
 });
-
+/*
 Template.main.helpers({
     posts() {
         return Posts.find({}, {sort: {likes: -1}});
     },
 });
+*/
+Template.main.helpers({
+    posts() {
+        var posts = Posts;
+        //console.log(Posts.findOne({}))
+        var keyword  = Session.get("search-query") ? Session.get("search-query") : null;
+        if(keyword !== null){
+            var query = new RegExp( keyword, 'i' );
+            var results = posts.find( { $or: [{'donator': query},
+                {'message': query}] } );
+            console.log("Finding")
+            if(results.count() !== 0){
+                return results;
+            }else{
+                return [{donator:"Žádné výsledky."}];
+            }
+        }else {
+            console.log("All")
+            return posts.find({}, {sort: {likes: -1}});
+        }
+    },
+});
+
 
 Template.main.events({
-  'click .like'(event, instance) {
-      event.preventDefault();
-      // increment the counter when button is clicked
-      const id = $(event.currentTarget).attr('id');
-      console.log(id);
-      Meteor.call('like', id, (data) => {
-          console.log("ZEMAN: majby dan");
-      });
-  },
+    'click .like'(event, instance) {
+        event.preventDefault();
+        // increment the counter when button is clicked
+        const id = $(event.currentTarget).attr('id');
+        console.log(id);
+        Meteor.call('like', id, (data) => {
+            console.log("ZEMAN: majby dan");
+        });
+    },
+    'keyup input.search-query': function (evt) {
+        if(evt.currentTarget.value.length >= 3){
+            Session.set("search-query", evt.currentTarget.value);
+        }else{
+            Session.set("search-query", null);
+        }
+    },
 });
